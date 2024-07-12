1d63127c2df471713fe0e6bf2465eb708160aecc
Fatih Culfa <mfthclf@gmail.com>
2024-07-12 09:35:34 +0300

    Pop-up menu is completed

--- app/src/main/java/com/mfthc/instaclonekotlin/FeedFragment.kt
+++ app/src/main/java/com/mfthc/instaclonekotlin/FeedFragment.kt
@@ -3,15 +3,19 @@ package com.mfthc.instaclonekotlin
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.navigation.Navigation
 import com.mfthc.instaclonekotlin.databinding.FragmentFeedBinding
 
 
-class FeedFragment : Fragment() {
+class FeedFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
 
     private var _binding: FragmentFeedBinding? = null
     private val binding get() = _binding!!
+    private lateinit var popup: PopupMenu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,16 @@ class FeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener { onClickFloatingButton(it) }
+        popup = PopupMenu(requireContext(), binding.floatingActionButton)
+        val inflater = popup.menuInflater
+        inflater.inflate(R.menu.popup_menu, popup.menu)
+        popup.setOnMenuItemClickListener(this)
+
+    }
+
+    fun onClickFloatingButton(view: View) {
+        popup.show()
 
     }
 
@@ -35,5 +49,17 @@ class FeedFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.uploadItem) {
+            val action = FeedFragmentDirections.actionFeedFragmentToUploadFragment()
+            Navigation.findNavController(requireView()).navigate(action)
+        } else {
+            val action = FeedFragmentDirections.actionFeedFragmentToUserFragment()
+            Navigation.findNavController(requireView()).navigate(action)
+        }
+        return true
+    }
+
 
 }
